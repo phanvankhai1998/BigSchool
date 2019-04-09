78308b3da48348ef877c0026a2ef564a6b316c0a
Phan Van Khai <Phan Van Khai@DESKTOP-5EKLDSL>
2019-04-09 08:40:25 +0700

    Init Model APS.NET

---
+++ BigSc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BigSc.Startup))]
+namespace BigSc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
